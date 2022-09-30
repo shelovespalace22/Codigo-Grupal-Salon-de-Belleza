3cd721f0b0628787866d6cdac759815a852f28b4
shelovespalace22 <miguelpokerafce@gmail.com>
2022-09-29 21:34:53 -0500

    mi parte

---
+++ miguel.py
@@ -0,0 +1,18 @@
+#Codigo de Miguel: Pagina principal y menu "Consultar Informacion de la Empresa."
+
+
+print("¡Bienvenido a Vain Women! El mejor salon de belleza de la ciudad.")
+print("1) Consultar informacion de la empresa.")
+print("2) Comprar un producto.")
+print("3) Agendar una cita ")
+print("4) Servicio al cliente ")
+menu=int(input("Por favor digite una de las opciones vistas anteriormente: "))
+if menu==1 :
+    print("Somos una empresa dedicada a ofrecer servicios de cuidado personal y estética, atendiendo a las demandas y necesidades de los clientes.")
+    print("Informacion de contacto:  ")
+    print("*Via telefonica: 3195199095")
+    print("*Correo electronico saladebelleza@gmail.com")
+    print("Encuentranos en las redes como: ")
+    print("*Facebook:  @saladebelleza")
+    print("*Instagram:  @saladebelleza01")
+    print("Encuentranos en la Av. de las Americas Carrera 6 # 8-45 local 102")
